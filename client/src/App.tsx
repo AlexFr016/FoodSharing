91a33efd05bc7d325eb1fb2f294ea03ffc185049
Roman <roman.sergeevich35@gmail.com>
2023-02-16 18:06:12 +0300

    navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Container } from '@mui/material';
+import NavigationBar from './components/ui/NavigationBar/NavigationBar';
 
 function App(): JSX.Element {
-  return <div>dfsdf</div>;
+  return (
+    <Container>
+      <NavigationBar />
+      <Routes>
+
+        
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
