bc26eb3cb4a19e893b85e9d9658ea2738b421325
Denis <den748@gmail.com>
2023-02-17 18:51:18 +0300

    pre-merge commit

---
+++ server/routes/authRouter.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const { User } = require('../db/models');
+
+
+const authRouter = express.Router();
+
+authRouter.post('/', async (req, res) => {
+    const {}
+})
