1972e3af2c38d27a29bef9a4d308afa0c3607057
Denis <den748@gmail.com>
2023-02-22 14:23:00 +0300

    haha

--- client/src/components/pages/ProfilePartnerPage/ProfilePartnerPage.tsx
+++ client/src/components/pages/ProfilePartnerPage/ProfilePartnerPage.tsx
@@ -1,4 +1,4 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import AboutPartnerProfileCard from '../../ui/AboutPartnerCard/AboutPartnerProfileCard';
 import CommentsSection from '../../ui/CommentsSection/CommentsSection';
@@ -23,10 +23,7 @@ export default function ProfilePartnerPage(): JSX.Element {
           <AboutPartnerProfileCard />
         </Grid>
       </Grid>
-      <Grid sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-        <OnePartnerRequestCard />
-        <OnePartnerRequestCard />
-      </Grid>
+      <Typography>Отзывы:</Typography>
       <CommentsSection />
     </Container>
   );
