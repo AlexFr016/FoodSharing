350cb4bb8772226b55783f8ca387831b6376bc98
Roman <roman.sergeevich35@gmail.com>
2023-02-23 12:54:13 +0300

    map

--- client/src/components/pages/MapRequestPage/MapRequestPage.jsx
+++ client/src/components/pages/MapRequestPage/MapRequestPage.jsx
@@ -7,17 +7,6 @@ import { useAppDispatch, useAppSelector } from '../../../redux/hooks';
 export default function MapRequestPage() {
   const [point, setPoint] = useState([]);
 
-  //   const map = new ymaps.Map('map', {
-  //     center: [59.94, 30.32],
-  //     zoom: 12,
-  //     controls: ['zoomControl'],
-  //     behaviors: ['drag']
-  //   });
-
-  // let placemark = new ymaps.Placemark([59.94, 30.32], {})
-
-  // map.geoObjects.add(placemark)
-
   useEffect(() => {
     axios('/api/post')
       .then((res) => {
@@ -27,7 +16,6 @@ export default function MapRequestPage() {
   }, []);
 
   useEffect(() => {
-    ymaps.ready(init);
     function init() {
       const myMap = new ymaps.Map(
         'map',
@@ -41,30 +29,27 @@ export default function MapRequestPage() {
       );
 
       point?.forEach((el) => {
-        ymaps
-          .geocode(el.address, {
-            results: 4,
-          })
-          .then((res) => {
-            //console.log(res);
-            const firstGeoObject = res?.geoObjects.get(0);
-            //console.log(firstGeoObject);
-            const coords = firstGeoObject?.geometry.getCoordinates();
-            //console.log(coords);
-            const placemark = new ymaps.Placemark(
-              coords,
-              {
-                balloonContentBody: '<p>Hello<p/>',
-              },
-              {
-                preset: 'islands#violetStretchyIcon',
-              },
-            );
+        ymaps.geocode(el.address, {}).then((res) => {
+          //console.log(res);
+          const firstGeoObject = res?.geoObjects.get(0);
+          //console.log(firstGeoObject);
+          const coords = firstGeoObject?.geometry.getCoordinates();
+          //console.log(coords);
+          const placemark = new ymaps.Placemark(
+            coords,
+            {
+              balloonContentBody: el.title,
+            },
+            {
+              preset: 'islands#blueGardenCircleIcon',
+            },
+          );
 
-            myMap?.geoObjects.add(placemark);
-          });
+          myMap?.geoObjects.add(placemark);
+        });
       });
     }
+    if (point.length) ymaps.ready(init);
   }, [point]);
   return <div id="map" style={{ minWidth: '900px', height: '700px' }} />;
 }
